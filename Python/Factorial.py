523aa3c35e4678fb06a7f5a7e56df28529f64c9a
Vaibhav Khandekar <32119720+vaibhau@users.noreply.github.com>
2023-09-11 12:29:53 +0530

    Update Factorial.py as per sample tests

--- Python/Factorial.py
+++ Python/Factorial.py
@@ -1,10 +1,3 @@
+import math
 n = int(input("enter any number = "))
-fact = 1
-if n<0:
-    print("factorial does not exist for negative numbers")
-elif n==0:
-    print("factorial of 0 is 1")
-else:
-    for i in range(1,n+1):
-        fact = fact*i
-print("factorial of",n,"is",fact)
+print(math.factorial(n))
