0c27b48680bb465ecb6e8ccdf7edd0ce3ae8834f
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2023-10-19 23:13:09 +0530

    Update Factorial.py

--- Python/Factorial.py
+++ Python/Factorial.py
@@ -1,3 +1,15 @@
+# Solution 1
+
 import math
 n = int(input())
 print(math.factorial(n))
+
+# Solution 2
+
+n = int(input())
+fact = 1
+if (n>=1) and (n<=10):
+    for i in range(1,n+1):
+        fact = fact * i
+
+print(fact)
